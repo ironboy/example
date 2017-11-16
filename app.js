5e87ce1f791bc4eb28f6020cc75959362b41df17
Thomas Frank <t@clientsmile.se>
2017-11-16 10:22:16 +0100

    Fixed var name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dir__ + '/www'));
+app.use(express.static(__dirname + '/www'));
 
 app.listen(3000, function () {
   console.log('Webserver listening on port 3000');
